aa8a8d8135cd6ce5af0a1548b37839eaef79803e
BrunoSilva <eps.bruno@outlook.com>
2025-04-24 20:45:34 -0300

    main.py conectaor a pasta teste.html

--- main.py
+++ main.py
@@ -6,15 +6,24 @@ app = FastAPI()
 # Configura CORS para seu frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://bnsilva.vercel.app"],  # URL exata do seu front
-    allow_methods=["GET", "POST"],
+    allow_origins=["*"],  # URL exata do seu front
+    allow_methods=["*"],  # Métodos HTTP permitidos
+    allow_credentials=True,
     allow_headers=["*"]
 )
 
-@app.get("/api/status")
-def status():
-    return {"status": "online", "framework": "FastAPI"}
+# Rota de teste
+@app.get("/api")
+def home():
+    return {"message": "Backend rodando!"}
 
-@app.get("/api/echo/{message}")
-def echo(message: str):
-    return {"original": message, "processed": message.upper()}gla
+# Rota para integrar com o frontend
+@app.get("/api/hello")
+def hello(name: str = "Bruno"):
+    return {"message": f"Olá, {name}! Sou o backend Python."}
+
+@app.get("/api/hello2")
+def hello2(name: str = None):
+    if name:
+        return {"message": f"oi, {name}! voce vai aprender."}
+    
